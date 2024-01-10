7d4fed7a10d6e733d97853daf03b555ab6ae9e8f
Lee Sehyeon <84698896+sehyeon518@users.noreply.github.com>
2024-01-10 16:01:06 +0900

    mod: json 저장 형식 수정
    json data
    RGB, HSV, CIELAB, RGB_feature, HSV_feature, LAB_feature, Hue_probability, Hue_Log_probability, Hue_adjacent_probability, Hue_Log_adjacent_probability, Hue_entropy, CH, Light_gradient, Hue_gradient, input_data, output_color

--- data_processing/ColorSuggestion/jsonl.py
+++ data_processing/ColorSuggestion/jsonl.py
@@ -44,9 +44,9 @@ with open(load_json_file, 'r') as f:
 
         hue_probabilities = []
         for color in HSV_H:
-            probabilities = hue_probability(color, HSV, threshold=30)
-            hue_probabilities.extend(probabilities)
-            
+            probabilities = hue_probability(color, HSV)
+            hue_probabilities.append(probabilities)
+
         adjacent_probabilities = []
         for i in range(len(RGB)):
             for j in range(i + 1, len(RGB)):
@@ -57,6 +57,7 @@ with open(load_json_file, 'r') as f:
         all_CH_values = []
         LCH_array = np.column_stack([LAB_L, LAB_A, HSV_H])
         for color1, color2 in itertools.combinations(LCH_array, 2):
+
             L1, C1, H1 = color1
             L2, C2, H2 = color2
             CH_value = calculate_CH(L1, C1, H1, L2, C2, H2)
@@ -68,18 +69,19 @@ with open(load_json_file, 'r') as f:
         data['RGB_feature'] = [calculate_statistics(RGB_R), calculate_statistics(RGB_G), calculate_statistics(RGB_B)]
         data['HSV_feature'] = [calculate_statistics(HSV_H), calculate_statistics(HSV_S), calculate_statistics(HSV_V)]
         data['LAB_feature'] = [calculate_statistics(LAB_L), calculate_statistics(LAB_A), calculate_statistics(LAB_B)]
-        data['Hue_probability'] = calculate_hsv_statistics(probabilities)
-        data['Hue_Log_probability'] = calculate_hsv_statistics(probabilities, True)
-        data['Hue_adjacent_probability'] = calculate_hsv_statistics(adjacent_probabilities)
-        data['Hue_Log_adjacent_probability'] = calculate_hsv_statistics(adjacent_probabilities, True)
-        data['Hue_entropy'] = calculate_hue_entropy(HSV)
-        data['CH'] = all_CH_values
-        data['Light_gradient'] = calculate_gradient(LAB_L)
-        data['Hue_gradient'] = calculate_gradient(HSV_H)
-        data['input_data'] = sum(data['RGB'], data['HSV'], data['CIELAB'], data['RGB_feature'], data['HSV_feature'], data['LAB_feature'], data['Hue_probability'], data['Hue_Log_probability'], data['Hue_adjacent_probability'], data['Hue_Log_adjacent_probability'], data['Hue_entropy'], data['CH'], data['Light_gradient'], data['Hue_gradient'], [])
+        data['Hue_probability'] = [calculate_hsv_statistics(hue_probabilities)]
+        data['Hue_Log_probability'] = [calculate_hsv_statistics(hue_probabilities, True)]
+        data['Hue_adjacent_probability'] = [calculate_hsv_statistics(adjacent_probabilities)]
+        data['Hue_Log_adjacent_probability'] = [calculate_hsv_statistics(adjacent_probabilities, True)]
+        data['Hue_entropy'] = [[calculate_hue_entropy(HSV)]]
+        data['CH'] = [all_CH_values]
+        data['Light_gradient'] = [calculate_gradient(LAB_L).tolist()]
+        data['Hue_gradient'] = [calculate_gradient(HSV_H).tolist()]
+        data['input_data'] = sum((data['RGB'], data['HSV'], data['CIELAB'], data['RGB_feature'], data['HSV_feature'], data['LAB_feature'], data['Hue_probability'], data['Hue_Log_probability'], data['Hue_adjacent_probability'], data['Hue_Log_adjacent_probability'], data['Hue_entropy'], data['CH'], data['Light_gradient'], data['Hue_gradient']), [])
+        data['input_data'] = [[element for sublist in data['input_data'] for element in sublist]]
+
         data['output_color'] = HSV_8[4].tolist()
 
-        print(data)
         with open(json_file_path, 'a') as jsonl_file:
             json.dump(data, jsonl_file)
             jsonl_file.write('\n')
