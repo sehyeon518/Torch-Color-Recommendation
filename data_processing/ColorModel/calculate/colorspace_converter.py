be7d1871e34fc332dae4258127c4b9746b742ba8
Lee Sehyeon <84698896+sehyeon518@users.noreply.github.com>
2024-01-15 10:36:13 +0900

    mod: lab color space converter update
    R(0~1.0), G(0~1.0), B(0~1.0)
    to L(0~100), A(-128~127), B(-128~127)

--- data_processing/ColorModel/calculate/colorspace_converter.py
+++ data_processing/ColorModel/calculate/colorspace_converter.py
@@ -28,36 +28,72 @@ def rgb_to_hsv(r, g, b):
     return round(H, 3), round(S*100, 3), round(V*100, 3)
 
 
-def rgb_to_lab(r, g, b):
-    r, g, b = r / 255.0, g / 255.0, b / 255.0
+def rgb_to_lab(rgb):
+    r, g, b = rgb
 
-    # Convert to XYZ
-    r = 100.0 * ((r + 0.055) / 1.055) ** 2.4 if (r > 0.04045) else (r / 12.92)
-    g = 100.0 * ((g + 0.055) / 1.055) ** 2.4 if (g > 0.04045) else (g / 12.92)
-    b = 100.0 * ((b + 0.055) / 1.055) ** 2.4 if (b > 0.04045) else (b / 12.92)
+    # Convert RGB to XYZ
+    r = 100.0 * (r / 255.0)
+    g = 100.0 * (g / 255.0)
+    b = 100.0 * (b / 255.0)
+
+    r = r / 100.0
+    g = g / 100.0
+    b = b / 100.0
+
+    if r > 0.04045:
+        r = ((r + 0.055) / 1.055) ** 2.4
+    else:
+        r = r / 12.92
+
+    if g > 0.04045:
+        g = ((g + 0.055) / 1.055) ** 2.4
+    else:
+        g = g / 12.92
+
+    if b > 0.04045:
+        b = ((b + 0.055) / 1.055) ** 2.4
+    else:
+        b = b / 12.92
+
+    r = r * 100.0
+    g = g * 100.0
+    b = b * 100.0
 
     x = r * 0.4124564 + g * 0.3575761 + b * 0.1804375
     y = r * 0.2126729 + g * 0.7151522 + b * 0.0721750
     z = r * 0.0193339 + g * 0.1191920 + b * 0.9503041
 
-    # Convert to LAB
-    x = x / 95.047
-    y = y / 100.000
-    z = z / 108.883
+    # Convert XYZ to LAB
+    x /= 95.047
+    y /= 100.000
+    z /= 108.883
 
-    x = x ** (1 / 3) if (x > 0.008856) else ((x * 903.3 + 16) / 116)
-    y = y ** (1 / 3) if (y > 0.008856) else ((y * 903.3 + 16) / 116)
-    z = z ** (1 / 3) if (z > 0.008856) else ((z * 903.3 + 16) / 116)
+    if x > 0.008856:
+        x = x ** (1.0 / 3.0)
+    else:
+        x = (903.3 * x + 16.0) / 116.0
 
-    L = max(0, 116 * y - 16)
-    a = (x - y) * 500
-    b = (y - z) * 200
+    if y > 0.008856:
+        y = y ** (1.0 / 3.0)
+    else:
+        y = (903.3 * y + 16.0) / 116.0
 
-    return round(L, 3), round(a, 3), round(b, 3)
+    if z > 0.008856:
+        z = z ** (1.0 / 3.0)
+    else:
+        z = (903.3 * z + 16.0) / 116.0
+
+    l = max(0.0, min(100.0, (116.0 * y) - 16.0))
+    a = max(-128.0, min(127.0, (x - y) * 500.0))
+    b = max(-128.0, min(127.0, (y - z) * 200.0))
+
+    return [l, a, b]
 
 
 if __name__ == "__main__":
-    rgb_values = (0, 0, 255)
+    rgb_values = (255, 0, 0)
     hsv_values = rgb_to_hsv(*rgb_values)
+    lab_values = rgb_to_lab(rgb_values)
     print("RGB:", rgb_values)
     print("HSV:", hsv_values)
+    print("LAB:", lab_values)
