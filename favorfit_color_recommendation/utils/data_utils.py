a58138fe54ed56a04f15f9dae3c480696ba71e3d
Lee Sehyeon <lifethis21@kookmin.ac.kr>
2024-01-24 09:40:21 +0900

    fix: top colors numpy array로 관리

--- favorfit_color_recommendation/utils/data_utils.py
+++ favorfit_color_recommendation/utils/data_utils.py
@@ -20,7 +20,7 @@ def get_top_indices_and_probabilities(output, list_of_colors, num_indices=4):
     sorted_indices = np.argsort(normalized_probabilities)[::-1]
     sorted_probabilities = normalized_probabilities[sorted_indices]
 
-    top_colors = [list_of_colors[idx] for idx in top_indices[sorted_indices]]
+    top_colors = np.array([list_of_colors[idx] for idx in top_indices[sorted_indices]])
 
     return top_colors, sorted_probabilities
 
